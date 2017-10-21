f09f1f298c028d3aaae583c7d87a7a29759e5316
Don McCurdy <don.r.mccurdy@gmail.com>
2017-10-20 22:09:12 -0700

    ImageBitmapLoader: Use web worker by default.

--- examples/js/loaders/ImageBitmapLoader.js
+++ examples/js/loaders/ImageBitmapLoader.js
@@ -2,7 +2,7 @@
  * @author thespite / http://clicktorelease.com/
  */
 
-function detectCreateImageBitmap() {
+function detectCreateImageBitmap ( optionsList ) {
 
 	var url = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg==';
 
@@ -21,12 +21,19 @@ function detectCreateImageBitmap() {
 
 		} ).then( function ( blob ) {
 
-			Promise.all( [
-				createImageBitmap( blob, { imageOrientation: 'none', premultiplyAlpha: 'none' } ),
-				createImageBitmap( blob, { imageOrientation: 'flipY', premultiplyAlpha: 'none' } ),
-				createImageBitmap( blob, { imageOrientation: 'none', premultiplyAlpha: 'premultiply' } ),
-				createImageBitmap( blob, { imageOrientation: 'flipY', premultiplyAlpha: 'premultiply' } )
-			] ).then( function () {
+			var pendingImages = [];
+
+			for ( var i = 0; i < optionsList.length; i ++ ) {
+
+				var pendingImage = optionsList[ i ] === undefined
+					? createImageBitmap( blob )
+					: createImageBitmap( blob, optionsList[ i ] );
+
+				pendingImages.push( pendingImage );
+
+			}
+
+			Promise.all( pendingImages ).then( function () {
 
 				resolve();
 
@@ -42,22 +49,72 @@ function detectCreateImageBitmap() {
 
 }
 
-var canUseImageBitmap = detectCreateImageBitmap();
-canUseImageBitmap.then( function () {
+var canUseImageBitmap = detectCreateImageBitmap( [ undefined ] );
 
-	console.log( 'THREE.ImageBitmapLoader: createImageBitmap() supported.' );
+var canUseImageBitmapOptions = detectCreateImageBitmap( [
+	{ imageOrientation: 'none', premultiplyAlpha: 'none' },
+	{ imageOrientation: 'flipY', premultiplyAlpha: 'none' },
+	{ imageOrientation: 'none', premultiplyAlpha: 'premultiply' },
+	{ imageOrientation: 'flipY', premultiplyAlpha: 'premultiply' }
+] );
 
-} ).catch( function () {
+/**
+ * Self-contained worker for fetching and decoding an image, returning an
+ * ImageBitmap to the main thread.
+ */
+var ImageBitmapWorker = function () {
 
-	console.warn( 'THREE.ImageBitmapLoader: createImageBitmap() not supported.' );
+	/* global self */
 
-} );
+	self.onmessage = function ( message ) {
+
+		fetch( message.data.url ).then( function ( response ) {
+
+			return response.blob();
+
+		} ).then( function ( blob ) {
+
+			return message.data.options === undefined
+				? self.createImageBitmap( blob )
+				: self.createImageBitmap( blob, message.data.options );
+
+		} ).then( function (imageBitmap) {
+
+			self.postMessage( imageBitmap );
+
+		} ).catch( function ( error ) {
+
+			console.error('THREE.ImageBitmapWorker: ' + error);
+
+			self.postMessage( error );
+
+		} );
+
+	};
+
+};
+
+var createAbsolutePath = function ( href ) {
+
+	if ( href.match( /(https?:)?\/\// ) ) return href;
+
+	var link = document.createElement( 'a' );
+	link.href = href;
+	return link.protocol + '//' + link.host + link.pathname + link.search + link.hash;
+
+};
 
 
 THREE.ImageBitmapLoader = function ( manager ) {
 
+	canUseImageBitmap.catch( function () {
+
+		console.warn( 'THREE.ImageBitmapLoader: createImageBitmap() not supported.' );
+
+	} );
+
 	this.manager = manager !== undefined ? manager : THREE.DefaultLoadingManager;
-	this.options = {};
+	this.options = undefined;
 
 };
 
@@ -66,6 +123,12 @@ THREE.ImageBitmapLoader.prototype = {
 	constructor: THREE.ImageBitmapLoader,
 
 	setOptions: function setOptions( options ) {
+
+		canUseImageBitmapOptions.catch( function () {
+
+			console.warn( 'THREE.ImageBitmapLoader: createImageBitmap() options not supported.' );
+
+		} );
 
 		this.options = options;
 		return this;
@@ -98,30 +161,43 @@ THREE.ImageBitmapLoader.prototype = {
 
 		}
 
-		fetch( url ).then( function ( res ) {
+		// Build a worker from an anonymous function body
+		var workerBlobURL = URL.createObjectURL( new Blob(
+			[ '(', ImageBitmapWorker.toString(), ')()' ],
+			{ type: 'application/javascript' }
+		) );
 
-			return res.blob();
+		var worker = new Worker( workerBlobURL );
 
-		} ).then( function ( res ) {
-
-			return createImageBitmap( res, scope.options );
-
-		} ).then( function ( res ) {
-
-			THREE.Cache.add( url, res );
-
-			if ( onLoad ) onLoad( res );
-
-			scope.manager.itemEnd( url );
-
-		} ).catch( function ( e ) {
-
-			if ( onError ) onError( e );
-
-			scope.manager.itemEnd( url );
-			scope.manager.itemError( url );
-
+		worker.postMessage( {
+			url: createAbsolutePath( url ),
+			options: scope.options
 		} );
+
+		worker.onmessage = function ( message ) {
+
+			var result = message.data;
+
+			if ( result instanceof ImageBitmap ) {
+
+				THREE.Cache.add( url, result );
+
+				if ( onLoad ) onLoad( result );
+
+				scope.manager.itemEnd( url );
+
+			} else {
+
+				if ( onError ) onError( result );
+
+				scope.manager.itemEnd( url );
+				scope.manager.itemError( url );
+
+			}
+
+		};
+
+		URL.revokeObjectURL( workerBlobURL );
 
 	}
 
